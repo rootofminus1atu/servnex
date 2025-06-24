7eedcea9cc664e59ac7dac0ee5fac575529c214d
RootOfMinus1ATU <s00240122@atu.ie>
2025-06-24 10:52:48 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+echo "rust app running on port 5000"
 /usr/local/bin/rust_app &
 
-cd /python_app/python_app && python3 app.py &
+echo "python app running on port 8000"
+cd /python_app && python3 app.py &
 
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
